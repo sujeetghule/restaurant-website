27cb5f45977c9626a98e6583deb9b9451009df11
sujeetghule <sujeetghule8@gmail.com>
2022-08-20 16:09:31 +0530

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React ,{ useState} from 'react';
+import React  from 'react';
 import {GiHamburgerMenu } from 'react-icons/gi'
 import {MdOutlineRestaurantMenu } from 'react-icons/md'
 import images from '../../constants/images'
